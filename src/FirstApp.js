e2703bd3489f8e8de3d2bf79e8c9da2c99220c5f
LauraSanchezPasaje <63804907+LauraSanchezPasaje@users.noreply.github.com>
2022-09-04 15:47:45 -0500

    Initial commit

---
+++ src/FirstApp.js
@@ -0,0 +1,12 @@
+import './FirstApp.css'
+ 
+const FirstApp = () => {
+    return (
+      <>
+      <h1> Título. </h1>
+      <span> Holaaaa uwu</span>
+      </>
+    )
+  }
+  
+  export default FirstApp
